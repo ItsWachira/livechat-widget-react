f8810d852cf7291ef9e061c3be65ce912695aa7c
ItsWachira <brianwachira79@gmail.com>
2023-02-05 14:05:13 +0300

    integrate chatwoot's livechat widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Livechat from './components/Livechat.js';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Livechat />
         <a
           className="App-link"
           href="https://reactjs.org"
